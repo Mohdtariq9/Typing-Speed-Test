8f1df1b1d80148057cc48a465f1b9fb0535fd83b
user.name: Mohd Tariq <user.email: sayyedtariq06@gmail.com>
2025-09-08 06:35:01 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,93 @@
+const quoteEl = document.getElementById('qoute');
+const inputEl = document.getElementById('input');
+
+const timeEl = document.getElementById('time');
+const wpmEl = document.getElementById('wpm');
+const accuracyEl = document.getElementById('accuracy');
+
+const restartBtn = document.getElementById('restart');
+
+let quote = '';
+let startTime;
+let timerInterval;
+
+// Fetch random quote
+async function getRandomQuote() {
+
+    const res = await fetch('https://api.quoteable.io/random');
+    const data = await res.json();
+    return data.content;
+
+}
+
+// Start test
+async function startTest() {
+
+    quote = await getRandomQuote();
+    quoteEl.textContent = quote;
+
+    inputEl.value = '';
+    inputEl.disabled = false;
+    inputEl.focus();
+
+    timeEl.textContent = '0';
+    wpmEl.textContent = '0';
+    accuracyEl.textContent = '0';
+
+    startTime = null;
+    clearInterval(timerInterval);
+
+}
+
+// Timer
+function startTimer() {
+
+    let seconds = 0;
+
+    timerInterval = setInterval(() => {
+
+        seconds++;
+        timeEl.textContent = seconds;
+
+    }, 1000);
+}
+
+// Typing event
+inputEl.addEventListener('input', () => {
+    const input = inputEl.value;
+
+    if (!startTime) {
+        startTime = new Date();
+        startTimer()
+    }
+
+    // If input matches quotes (done typing)
+    if (input === quote) {
+        clearInterval(timerInterval);
+        const endTime = new Date();
+        const timeTaken = (endTime - startTime) / 1000;
+
+        const words = quote.split(' ').length;
+        const wpm = Math.round((words / timeTaken) * 60);
+        wpmEl.textContent = wpm;
+
+        // Accuracy
+        let correctChars = 0;
+        for (let i = 0; i < quote.length; i++) {
+            if (quote[i] === input[i]) {
+                correctChars++;
+            }
+        }
+        const accuracy = Math.round((correctChars / quote.length) * 100);
+        accuracyEl.textContent = accuracy;
+
+        inputEl.disabled = true;
+
+    }
+});
+
+// Restart
+restartBtn.addEventListener('click', startTest);
+
+// Start on load
+startTest();
